dacf738c2d0b012fcaf5ee79db597b008262da6a
aoyano <ayan.nurgaliyev.123456789@gmail.com>
2022-05-27 22:08:06 +0600

    heroku

--- app.js
+++ app.js
@@ -25,7 +25,24 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
+app.set('trust proxy', 1);
 
+app.use(session({
+    cookie:{
+        secure: true,
+        maxAge:60000
+    },
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use(function(req,res,next){
+    if(!req.session){
+        return next(new Error('Oh no')) //handle error
+    }
+    next() //otherwise continue
+});
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
